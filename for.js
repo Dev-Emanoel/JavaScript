ba7445d03961cb5f5b49051bbd7f54bb18c07571
Emanoel Antonio da Silva <dev.emanoel@protonmail.com>
2020-05-10 22:48:27 -0300

    Update for.js
    Utilizei o 'continue' para a função não parar quando encontrar o que procurava.

--- for.js
+++ for.js
@@ -4,10 +4,17 @@ const minhaMochila =[
     'mouse sem fio',
     'escova de dentes',
     'perfume',
-    'alcool em gel'
+    'alcool em gel',
+    'chocolate'
 ]
 for (let index = 0; index < minhaMochila.length; index++) {
     console.log(`${index+1}.${minhaMochila[index]}`) 
-    //if (minhaMochila[index]== 'escova de dentes') break
+    if (minhaMochila[index]== 'escova de dentes') break
     //parar de procurar quando encontrar a escova de dentes.
+}
+
+for (let index = 0; index < minhaMochila.length; index++) { 
+    if (minhaMochila[index]== 'escova de dentes') continue
+    console.log(`${index+1}.${minhaMochila[index]}`)
+    //quando ele passar pela escova de dentes ele força a continuação sem passar pelo console.
 }
