f005f7b30a2778ee47142056a07a3f0fdefcb1eb
Emanoel Antonio da Silva <dev.emanoel@protonmail.com>
2020-06-25 16:34:55 -0300

    Create call.js
    Criado o método call().

---
+++ call.js
@@ -0,0 +1,22 @@
+/*O método call chama uma função com um determinado valor e
+argumentos fornecidos individualmente.*/
+
+let name = {
+    firstName: 'Emanoel',
+    lastName: 'Antonio',
+}
+
+let printfullName = function(cidadeNatal){
+    console.log(this.firstName 
+        + " " + this.lastName 
+        + "," + " Cidade Natal: " 
+        + cidadeNatal)
+}
+
+let name2 = {
+    firstName: 'Lucas',
+    lastName: 'Emmanuel',
+}
+
+printfullName.call(name2, 'Jaboatão')
+printfullName.call(name, "Cabo")
